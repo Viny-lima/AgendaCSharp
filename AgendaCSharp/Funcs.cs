452484deb7ad28826087d451323fcc6b38d226a7
Viny-lima <viniciuslima.vf4@hotmail.com>
2021-09-21 18:10:45 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaCSharp/Funcs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaCSharp
+{
+    class Funcs
+    {
+        static int Menu()
+        {
+            Console.WriteLine("=============================");
+            Console.WriteLine("            Agenda           ");
+            Console.WriteLine("=============================");
+
+            Console.WriteLine("[1] Busca por primeiro nome");
+            Console.WriteLine("[2] Busca por nome completo");
+            Console.WriteLine("[3] Busca por tipo de contato");
+            Console.WriteLine("[4] Busca por cidade");
+            Console.WriteLine("[5] Inserir um contato");
+            Console.WriteLine("[6] Remover um contato");
+
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            return opcao;
+        }
+
+
+        static void SearchFirstName()
+        {
+
+        }
+
+        static void SerchFullName(object agenda)
+        {
+
+        }
+
+        static void SerchTypeContact(object agenda)
+        {
+
+        }
+
+        static void SerchCity(object agenda)
+        {
+
+        }
+
+        static void AddAgenda(object agenda)
+        {
+
+        }
+
+        static void RmAgenda(object agenda)
+        {
+
+        }
+
+    }
+}
---
+++ AgendaCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AgendaCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando variáveis do Sistema
+
+            struct Contato
+            {
+
+            }
+
+            
+        }
+    }
+}
